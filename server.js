930c8f77d8cb8062417dee2f01a979661c0bbaf6
Chariot7 <brennan.c.lowe@gmail.com>
2022-08-29 21:46:38 -0700

    ssl

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-import sslRedirect from 'heroku-ssl-redirect';
+let sslRedirect = require('heroku-ssl-redirect')
 const app = express();
 const publicPath = path.join(__dirname, '..','public');
 const port = process.env.PORT || 3000;
