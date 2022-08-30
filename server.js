0c312879301885f643c61f4f79963e8367302723
Chariot7 <brennan.c.lowe@gmail.com>
2022-08-29 21:34:00 -0700

    https

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const app = express();
 const publicPath = path.join(__dirname, '..','public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
+
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https'){
         res.redirect(`https://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
+        console.log('hitting')
+    }
+    else
+    next()
+})
+  
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
