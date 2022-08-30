21f4df2b90349c85dce5875ad18ab612874a6f4f
Chariot7 <brennan.c.lowe@gmail.com>
2022-08-30 01:58:31 -0700

    spiral size adjustments

--- src/pages/Spiral.js
+++ src/pages/Spiral.js
@@ -29,32 +29,38 @@ const spiral = (props) => {
         angleX = p5.map(p5.mouseX, 0, p5.width, 0, 2*p5.PI*1000)/1000;
         angleY = p5.map(p5.mouseY, 0, p5.height, 0, 2*p5.PI*1000)/1000;
 		p5.background(30);
-        p5.ambientLight(90)
+        p5.ambientLight(100)
 
-        p5.pointLight(165, 50, 190, -p5.mouseX/3, p5.mouseY/3, 320)
+        p5.pointLight(165, 50, 190, -p5.width/27, p5.height/27, 320)
   
-        p5.pointLight(p5.map(p5.mouseX, 0, p5.width, 190, 255), 200, 60, p5.mouseX/3, p5.mouseY/3, 220)
+        p5.pointLight(p5.map(p5.mouseX, 0, p5.width, 190, 255), 200, 60, p5.width/25, p5.height/25, 220)
+        p5.pointLight(p5.map(p5.mouseX, 0, p5.width, 190, 255), 200, 60, p5.height/15, p5.height/25)
+        p5.pointLight(p5.map(p5.mouseX, 0, p5.width, 190, 255), 200, 60, p5.height -p5.width/15,p5.height - p5.height/15, p5.height/25)
         p5.noStroke()
         p5.specularMaterial(250)
         p5.shininess(200)
 
         let angleArrX = [ -angleX, -angleY, angleX, -angleY, -angleX, angleY, -angleY, - angleX, -angleX , -angleY, angleX, angleY]
-        
+        // console.log(p5.mouseX/2000, p5.mouseY/2000)
+        let mouseX = p5.mouseX
+        let mouseY = p5.mouseY
+        let numX = p5.map(mouseX/2000, 0, 1.4, 0.6, 0.8)
+        let numY = p5.map(mouseY/2000, 0, 1.4, 0.6, 0.8)
         for(let i = 0 ; i < num * 12; i += 2*num){
             p5.push()
             p5.rotateX(angleArrX[i/num] )
             p5.rotateY(angleArrX[i/num + 1] )
             
-                let boxSize = p5.height * 0.925 /2
-                p5.scale(p5.mouseX/800, p5.mouseY/1200)
-                p5.box(boxSize+p5.mouseX/3, boxSize + p5.mouseY/6, boxSize,200,200)
+            p5.scale(numX, numY)
+                let boxSize = (p5.height * 0.925 /2) - p5.width/80
+                p5.box(boxSize+mouseX/2, boxSize + mouseY/3, boxSize/2,200,200)
                 p5.push()
                 
                 p5.rotateX(angleArrX[i/num] * -1)
                 p5.rotateY(angleArrX[i/num + 1] * -1)
                 p5.stroke(4,200,200)
                 
-                p5.box(boxSize+p5.mouseX/3 , boxSize + p5.mouseY/6 , boxSize,200,200)
+                p5.box(boxSize+mouseX/2 , boxSize + mouseY/3 , boxSize/2,200,200)
                 p5.pop()
             
             p5.pop()
