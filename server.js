19e200a3e7ba55c749da6bee88125b0879413ee0
Chariot7 <brennan.c.lowe@gmail.com>
2022-08-29 22:18:11 -0700

    test

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const express = require('express');
-// import sslRedirect from 'heroku-ssl-redirect';
-const app = express();
-const publicPath = path.join(__dirname, '..','public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+// const path = require('path');
+// const express = require('express');
+// // import sslRedirect from 'heroku-ssl-redirect';
+// const app = express();
+// const publicPath = path.join(__dirname, '..','public');
+// const port = process.env.PORT || 3000;
+// app.use(express.static(publicPath));
 
-// app.use(sslRedirect());
-app.get('/', function(req, res, next) {
-   console.log('something')
-   if (req.protocol == 'http') {
-       res.redirect('https://' +
-       req.get('host') + req.originalUrl);
-   }
-});
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!', port);
-});
+// // app.use(sslRedirect());
+// app.get('/', function(req, res, next) {
+//    console.log('something')
+//    if (req.protocol == 'http') {
+//        res.redirect('https://' +
+//        req.get('host') + req.originalUrl);
+//    }
+// });
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(publicPath, 'index.html'));
+// });
+// app.listen(port, () => {
+//    console.log('Server is up!', port);
+// });
