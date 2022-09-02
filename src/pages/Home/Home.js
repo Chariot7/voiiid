511ea9e2805bca99ce6f0491c4b81d3a89791609
Chariot7 <brennan.c.lowe@gmail.com>
2022-09-02 02:40:35 -0700

    test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,15 @@ import "./home.css";
 
 const home = function () {
   document.body.style.overflow = "auto";
-  console.log(document.getElementsByClassName('page')[0].offsetHeight)
+  let element = document.getElementsByClassName('page');
+  console.log(element[0])
+  element[0] ? element = element[0].offsetHeight : element = element[0]
   return (
     <>
       <div className="page">
         <h2>LG - Frieze, New York</h2>
         <h5>window innerHeight: {window.innerHeight}</h5>
-        <h5>div at 1oovh - 50px: {document.getElementsByClassName('page')[0].offsetHeight}</h5>
+        <h5>div at 1oovh - 50px: {element? element: 'no element'} </h5>
         <div className="col-3">
           <img src={img2} alt="artwork" className="screen" />
         </div>
