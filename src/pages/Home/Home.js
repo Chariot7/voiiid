e7cb3377e88015b4da17964498faef8973cc06f4
Chariot7 <brennan.c.lowe@gmail.com>
2022-09-02 02:07:16 -0700

    test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,13 @@ import "./home.css";
 
 const home = function () {
   document.body.style.overflow = "auto";
+  console.log(document.getElementsByClassName('page')[0].offsetHeight)
   return (
     <>
       <div className="page">
         <h2>LG - Frieze, New York</h2>
-        <h5>StanbyME Web App : Design & Build</h5>
+        <h5>window innerHeight: {window.innerHeight}</h5>
+        <h5>div at 1oovh - 50px: {document.getElementsByClassName('page')[0].offsetHeight}</h5>
         <div className="col-3">
           <img src={img2} alt="artwork" className="screen" />
         </div>
