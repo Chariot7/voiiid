0f76bc798d7692c803f5afab070ae6dbbd53d76a
Chariot7 <brennan.c.lowe@gmail.com>
2023-01-16 08:45:11 -0800

    home changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,5 @@
 import img from "../../Assets/LG_Frieze_Booth.png";
 import img2 from "../../Assets/StanbyME.png";
-import img3 from "../../Assets/LG1.png";
-import img4 from "../../Assets/LG2.png";
-import img5 from "../../Assets/LG3.png";
 import "./home.css";
 
 const home = function () {
@@ -12,23 +9,12 @@ const home = function () {
   return (
     <>
       <div className="page">
-        <h2>LG - Frieze, New York (UNDER CONSTRUCTION)</h2>
-        <h5>window innerHeight: {window.innerHeight}</h5>
-        <h5>div at 1oovh - 50px: {element ? element : "no element"} </h5>
+        <h2>LG - Frieze, New York (Website - UNDER CONSTRUCTION)</h2>
         <div className="col-3">
           <img src={img2} alt="artwork" className="screen" />
         </div>
         <div className="col-8">
           <img src={img} alt="artwork" className="booth" />
-        </div>
-        <div className="col-8">
-          <img src={img3} alt="artwork" className="booth" />
-        </div>
-        <div className="col-8">
-          <img src={img4} alt="artwork" className="booth" />
-        </div>
-        <div className="col-8">
-          <img src={img5} alt="artwork" className="booth" />
         </div>
       </div>
     </>
